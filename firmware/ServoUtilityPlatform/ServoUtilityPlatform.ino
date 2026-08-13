3752844cb145a9af02ca2395143fb95fa8051c91
Denkata7622 <150596050+Denkata7622@users.noreply.github.com>
2026-08-14 00:32:35 +0300

    Refactor: - Working OLED Menu with sections - - Manual Control - - Calibration - - Debug - - Settings (Still in Progress)
    - Moved the working rotation prototype to its own "Manual Control" option
    - Added LASER Calibration which works by attaching a laser to your servo head and pointing it to a marking on your wall (preferably on a paper if you dont want to permamently have a marking to calibrate your servos XD), then it rotates a certain number of degrees and you measure the distance from the mark. The code takes the distance from the servo to the wall and the one from the marking to the laser dot (both measured manually) and calculates the real angle that corresponds to the digital one.
    - Added a Debugger that shows the joysticks offCenter values (for debugging purposes and mainly to see if there's a problem with the wires, which happens occasionally)
    - Added a cool scrollbar at the side with a wheel that moves based on the focused option
    - Made it so each menu option only uses 1 row and never goes to the next (even when focused)

--- firmware/ServoUtilityPlatform/ServoUtilityPlatform.ino
+++ firmware/ServoUtilityPlatform/ServoUtilityPlatform.ino
@@ -2,6 +2,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Math.h>
 
 // Define Screen Parameters
 #define SCREEN_WIDTH 128
@@ -9,41 +10,77 @@
 #define OLED_RESET -1
 #define SCREEN_ADDRESS 0x3C
 
-// Create a Servo Object
-
 // Hardware Pins
-const int servoPin = 10;
-const int yPin = 15;
-const int xPin = 16;
+const int servoPin = 38;
+const int yPin = 1;
+const int xPin = 2;
 const int buttonPin = 12;
 
 // Joystick offCenter Calibration Values
 double xOffCenterCalibration = 0;
 double yOffCenterCalibration = 0;
 
-// Servo Angle
-double angle = 90;
+// Option to invert joystick directions
+bool axisInverted = true;
+
+// Servo Variables
+double angle = 90; // Servo Angle
 double minAngle = 0; // Using double for future servo calibration function
 double maxAngle = 180; // Using double for future servo calibration function
 double speed = 0; // Angles moved every Loop()
-int maxSpeed = 4;
 int loopDelay = 10;
-double speedMultiplier = 1; // Change this when changing the loopDelay to keep the same ratio without fine tuning the whole Quad function again
 
 // Joystick Speed Variables
-int offCenterSquaredMultiplier = 1;
-int offCenterLinearMultiplier = 14;
-double offCenterAddition = -1.25;
+double offCenterSquaredMultiplier = 8;
+int offCenterLinearMultiplier = 3;
+double offCenterAddition = -1;
+double speedMultiplier = 1; // Change this when changing the loopDelay to keep the same ratio without fine tuning the whole Quad function again
+int maxSpeed = 10;
 
 // analogRead() to offCenter ratio
 double joystickScaleDivider = 2048; // Use 2048 for esp32 and 1024 for Arduino
 
-// Array for menu options
-String menuOptions[] = {"Manual Control", "Calibration", "Debug", "Settings"};
-int menuLength = sizeof(menuOptions) / sizeof(menuOptions[0]);
+// Main Menu Options
+String mainMenuOptions[] = {"Manual Control", "Calibration", "Debug", "Settings"};
+int mainMenuLength = sizeof(mainMenuOptions) / sizeof(mainMenuOptions[0]);
+
+// Calibration Menu Options
+String calibrationMenuOptions[] = {"Simple Calibr.", "Pulse Width Calibr.", "C.E.A. LASER Calibr.", "360deg Servo", "180deg Servo", "Go Back"}; // Some options are just for example!
+int calibrationMenuLength = sizeof(calibrationMenuOptions) / sizeof(calibrationMenuOptions[0]);
+
+// Universal Menu Variables
 int selected = 0;
 int firstVisible = 0;
 int maxOptions = 6; // Maximum options that can fit on the screen at the same time. Generic 128x64 displays can fit ~7 at setTextSize(1)
+int maxCharsPerLine = 20;
+
+// ########################
+// Servo Profile Preferences:
+// ########################
+
+// Basic Profile:
+// [0, 45, 90, 135, 180]
+double SavedAnglesBasic[3][6]= {{0,0,0,0,0,0}, // made with 6 instead of 5 because SavedAngles[i] = SavedAngles[i - 1] + the angle on top of that
+                                {0,0,0,0,0,0},
+                                {0,0,0,0,0,0}};
+// Accurate Profile:
+// [0,
+//  22.5,  45,   67.5,  90,
+//  112.5, 135,  157.5, 180
+// ]
+double SavedAnglesAccurate[3][10]= {{0,0,0,0,0,0,0,0,0,0}, // made with 10 instead of 9 because SavedAngles[i] = SavedAngles[i - 1] + the angle on top of that
+                                    {0,0,0,0,0,0,0,0,0,0},
+                                    {0,0,0,0,0,0,0,0,0,0}};
+// Servophile Profile:
+// [0, 
+//  11.25,  22.5,  33.75,  45,
+//  56.25,  67.5,  78.75,  90, 
+//  101.25, 112.5, 123.75, 135, 
+//  146.25, 157.5, 168.75, 180
+// ]
+double SavedAnglesServophile[3][14]= {{0,0,0,0,0,0,0,0,0,0,0,0,0,0}, // made with 14 instead of 13 because SavedAngles[i] = SavedAngles[i - 1] + the angle on top of that
+                                      {0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                      {0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
 
 // Make a display
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
@@ -67,6 +104,7 @@ void setup()
   display.setCursor(30,15);
   display.println("S.U.P.");
   display.setTextSize(1);
+  display.setCursor(5, 32);  
   display.println("Developed by Kacenta");
 
   display.display();
@@ -79,11 +117,17 @@ void setup()
 
 void loop()
 {
-  buildMenu(firstVisible, selected);
+  buildMenu(firstVisible, selected, mainMenuOptions, mainMenuLength);
 
   // Use the Joystick's Y axis to navigate
   int yReading = analogRead(yPin);
   double yOffCenter = calculateOffCenter(yReading);
+
+  if (axisInverted)
+  {
+    yOffCenter = -yOffCenter;  
+  }
+
   String yDirection = offCenterToDirection(yOffCenter, 1);
 
   // Read if button has been pressed (first checks for button on purpose)
@@ -91,17 +135,17 @@ void loop()
 
   if (!buttonState)
   {
-    callMenuOption(selected);
+    callMenuOption(0, selected);
   }
 
   // Update Y based on yDirection
-  dirUpdateYselected(yDirection);
+  dirUpdateYselected(yDirection, mainMenuLength);
 
   delay(50);
 }
 
 
-void buildMenu(int firstOption, int focusedOption)
+void buildMenu(int firstVisibleOption, int focusedOption, String menuOptions[], int menuLength)
 {
   display.clearDisplay();
   display.setCursor(0, 0);
@@ -112,44 +156,136 @@ void buildMenu(int firstOption, int focusedOption)
 
   for (int i = 0; i < visibleOptions; i++)
   {
+    int currentOption = i + firstVisibleOption;
+    String menuOption = menuOptions[currentOption];
     display.setTextColor(WHITE);
 
-    if (i == focusedOption)
+    if (currentOption == focusedOption)
     {
+      menuOption = String("> ") + menuOption;
       display.setTextColor(BLACK, WHITE);
     }
 
-    display.println(menuOptions[i]);
+    // Make sure that each option fits in its own line (configure maxCharsPerLine to your screen)
+    // TODO: make a scroll animation
+    // For now - only trim
+    menuOption = menuOption.substring(0, maxCharsPerLine);
+
+
+    display.println(menuOption);
   }
+
+  drawScrollbar(focusedOption, menuLength);
+
   display.display();
 }
 
-void callMenuOption(int selectedOption)
+void drawScrollbar(int focusedOption, int menuLength)
 {
-  switch (selectedOption)
+  for(int i = 0; i < SCREEN_HEIGHT / 4 ; i++)
+  {
+    display.drawLine(SCREEN_WIDTH - 4, i * 4 + 2, SCREEN_WIDTH - 4, i * 4 + 4, WHITE);
+  }
+
+  for(int i = 0; i < menuLength; i++)
+  {
+    if (i == focusedOption)
+    {
+      if (i == menuLength - 1)
+      {
+        display.fillRect(SCREEN_WIDTH - 6, SCREEN_HEIGHT - 8, 5, 8, WHITE);
+        continue;
+      }
+      else if (i == firstVisible)
+      {
+        display.fillRect(SCREEN_WIDTH - 6, 0, 5, 8, WHITE);
+        continue;
+      }
+      else 
+      {
+        display.fillRect(SCREEN_WIDTH - 6, i * (SCREEN_HEIGHT - 8) / (menuLength - 1), 5, 8, WHITE);
+      }
+    }
+  }
+}
+
+void callMenuOption(int contextMenu, int selectedOption)
+{
+  switch (contextMenu)
   {
     case 0:
-      callManualControl();
+      switch (selectedOption)
+      {
+        case 0:
+          callManualControl();
+          break;
+
+        case 1:
+          callCalibrationMenu();
+          break;
+
+        case 2:
+          callDebug();
+          break;
+
+        case 3:
+          //callSettings();
+          break;
+
+        case 4:
+          //callAbout();
+          break;
+
+        default: 
+          Serial.println("Wrong Option Called");
+          Serial.print(selectedOption);
+          break;
+      }
       break;
 
     case 1:
-      //callCalibrate();
+      switch (selected)
+      {
+        case 0:
+          break;
+        
+        case 1:
+          break;
+
+        // Laser Calibration
+        case 2:
+          callLaserCalibration();
+          break;
+
+        case 3:
+          break;
+
+        case 4:
+          break;
+
+        // Go Back
+        case 5:
+          break;
+
+        default:
+          break;
+      }
       break;
 
     case 2:
-      callDebug();
-      break;
-
-    case 3:
-      //callSettings();
-      break;
-
-    default: 
-      Serial.println("Wrong Option Called");
-      Serial.print(selectedOption);
+      switch (selected)
+      {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        default:
+          break;
+      }
       break;
   }
 }
+
 
 void callDebug()
 {
@@ -178,7 +314,7 @@ double calculateOffCenter(int ValueInput)
 String offCenterToDirection(double axisOffCenter, int Axis)
 {
   String yDirection[] = {"UP", "DOWN", "NONE"};
-  String xDirection[] = {"LEFT", "RIGHT", "NONE"};
+  String xDirection[] = {"RIGHT", "LEFT", "NONE"};
 
   // Set to NONE by default
   int dirSelect = 2;
@@ -219,7 +355,7 @@ void dirUpdateXselected(String xDirection)
 }
 
 
-void dirUpdateYselected(String yDirection)
+void dirUpdateYselected(String yDirection, int menuLength)
 {
   // Actually DOWN makes "selected" go UP, because the list starts at 0 and INCREASES as it goes DOWN
   if (yDirection == "DOWN")
@@ -227,18 +363,18 @@ void dirUpdateYselected(String yDirection)
     selected++;
     delay(180);
   }
-  // UP makes "selected" go DOWN, because the list ends at menu[menuLenght - 1] and DECREASES as it goes UP
+  // UP makes "selected" go DOWN, because the list ends at menu[menuLength - 1] and DECREASES as it goes UP
   else if (yDirection == "UP")
   {
     selected--;
     delay(180);
   }
 
-  fixSelected();
+  fixSelected(menuLength);
 }
 
 // Check that keeps "selected" in range 0 to (menuLength - 1)
-void fixSelected()
+void fixSelected(int menuLength)
 {
   if (selected < 0)
   {
@@ -260,8 +396,15 @@ void debuggerFunction()
     int yReading = analogRead(yPin);
     int xReading = analogRead(xPin);
     double xOffCenter = calculateOffCenter(xReading);
-    String xDirection = offCenterToDirection(xOffCenter, 0);
     double yOffCenter = calculateOffCenter(yReading);
+
+    if (axisInverted)
+    {
+      xOffCenter = -xOffCenter;
+      yOffCenter = -yOffCenter;
+    }
+
+    String xDirection = offCenterToDirection(xOffCenter, 0);
     String yDirection = offCenterToDirection(yOffCenter, 1);
 
     display.clearDisplay();
@@ -302,7 +445,7 @@ double offCenterToAngleChange(double xOffCenter)
     sign = 1;
   }
 
-  // Use a Quadratic Equation for Linear accereration + slight boost at the end
+  // Use a Quadratic Equation for Linear accereration + Quadratic boost near the ends
   speed = (sign * (xOffCenter * xOffCenter) * offCenterSquaredMultiplier) + (xOffCenter * offCenterLinearMultiplier) + (sign * offCenterAddition);
   
   // Check if offCenterAddition causes the speed to change sign 
@@ -392,5 +535,263 @@ void manualControl()
       return;
     }
     delay(loopDelay);
+  }
+}
+
+void callCalibrationMenu()
+{
+  delay(300);
+
+  calibrationMenu();
+
+  delay(300);
+}
+
+void calibrationMenu()
+{
+  selected = 0;
+  firstVisible = 0;
+
+  while(true)
+  {
+    buildMenu(firstVisible, selected, calibrationMenuOptions, calibrationMenuLength);
+
+    // Use the Joystick's Y axis to navigate
+    int yReading = analogRead(yPin);
+    double yOffCenter = calculateOffCenter(yReading);
+
+    // TODO Make a function invertAxis(int yesX,int yesY)
+    if (axisInverted)
+    {
+      yOffCenter = -yOffCenter;  
+    }
+
+    // Convert offCenter to "UP" or "DOWN"
+    String yDirection = offCenterToDirection(yOffCenter, 1);
+
+    // Read if button has been pressed (first checks for button on purpose)
+    int buttonState = digitalRead(buttonPin);
+    if (!buttonState)
+    {
+      // Keep "Go Back" last so it always works
+      if (selected == calibrationMenuLength - 1)
+      {
+        // Go Back
+        return;
+      }
+
+      callMenuOption(1, selected);
+    }
+
+    // Update Y based on yDirection
+    dirUpdateYselected(yDirection, calibrationMenuLength);
+
+    delay(50);
+  }
+}
+
+
+
+void callLaserCalibration()
+{
+  delay(300);
+
+  laserCalibration();
+
+  delay(300);
+}
+
+
+//
+// Use tg to calc the angle
+// example:
+// 200cm Distance from Laser to wall
+// 10cm Distance from 0 deg to ? deg (in the code it's set to 15 deg)
+// tg of needed angle = 10 / 200 = 0.05
+// use atan(tg) to get angle in radians
+// * 180 / Pi to convert to angles
+// 
+// Made to be Reusable (Unlike toilet paper) <3
+//
+double calcDistanceToAngle(double startingDistance, double distance)
+{
+  double angleTan = distance / startingDistance;
+  double angleAtan = atan(angleTan);
+  double angle = convertRadiansToDegrees(angleAtan);
+
+  return angle;
+}
+
+// Multiplies by (180 / PI) to convert radians to angles
+double convertRadiansToDegrees(double radians)
+{
+  double angle = radians * 180 / PI;
+  return angle;
+}
+
+
+// 
+void laserCalibration()
+{
+  //
+  // TODO: Call a menu before starting and initialise arrays based on List chosen
+  // chooseModeMenu()
+  //
+
+  // Saved Servo Variables
+  double startingDistance = 0;
+  double Distances[5] = {0, 0, 0, 0, 0};
+  // saved angles is at the top cuz its used later
+  int distancesLength = sizeof(Distances) / sizeof(Distances[0]);
+
+  // Wait until user clicks and save distance
+  startingDistance = getLaserToWallDistance();
+
+  // Wait a bit to prevent accidental clicks 
+  delay(300);
+
+  // distance from servo to wall in cm
+  double distance = 0;
+
+  for (int i = 0; i < distancesLength; i++)
+  {
+    // RESET distance
+    distance = 0;
+
+    // Basic list of angles [0, 45, 90, 135, 180]
+    int servoAngle = i * 45;
+
+    // Set the servo to the corresponding angle 
+    TestServo.write(servoAngle);
+
+    // wait till user clicks
+    while (true)
+    {
+      int yReading = analogRead(yPin);
+      double yOffCenter = calculateOffCenter(yReading);
+      double speed = offCenterToAngleChange(yOffCenter); 
+      distance += (speed * speedMultiplier * 0.1); // TODO Change 0.2 with Y axis speed scale
+
+      String Message = String("Input ") + (i * 45) + ("deg distance");
+      
+      // Update Screen to Show Results
+      displayDistanceAndSpeedResult(distance, speed, Message);
+
+
+      int buttonState = digitalRead(buttonPin);
+      if (!buttonState)
+      {
+        // Save distance
+        Distances[i] = distance;
+
+        // Continue
+        break;
+      }
+    }
+    delay(300);
+  }
+
+  //
+  // Show saved distances at the end
+  // TODO: Separate with a function.
+  display.clearDisplay();
+  display.setCursor(0,0);
+
+  display.println("startingDistance: ");
+  display.println(startingDistance);
+
+  display.display();
+  
+  delay(1500);
+
+  display.clearDisplay();
+  display.setCursor(0,0);
+
+  display.println("Distances: ");
+  display.println("0, 45, 90, 135, 180");
+  display.println(Distances[0]);
+  display.println(Distances[1]);
+  display.println(Distances[2]);
+  display.println(Distances[3]);
+  display.println(Distances[4]);
+  display.display();
+  //
+  //
+  //
+
+  delay(2000);
+
+  display.clearDisplay();
+  display.setCursor(0,0);
+
+  display.println("Angles Saved:");
+
+  double SavedAngles[100] = {0};
+
+  // Use the Distances[] array to produce an Angles[] array
+  for (int i = 0; i < distancesLength; i++)
+  {
+    SavedAngles[i + 1] = calcDistanceToAngle(startingDistance, Distances[i])  + SavedAngles[i];
+    display.print(i * 45);
+    display.print(": ");
+    display.println(SavedAngles[i + 1]);
+    SavedAnglesBasic[0][i + 1] = SavedAngles[i + 1]; // for now we only use basic
+  }
+
+  // TODO: save to correct SavedAngles array based on angle array choice
+
+  display.display();
+
+  delay(3000);
+}
+
+void displayDistanceAndSpeedResult(double inputDistance, double inputSpeed, String TopMessage)
+{
+  display.clearDisplay();
+  display.setCursor(0, 0);
+
+  // Set up Messages 
+  String distanceMessage = String("Distance (cm): ") + inputDistance;
+
+  // Print everything to screen
+  display.println(TopMessage);
+  display.println(distanceMessage);
+  display.println(String("Speed: ") + inputSpeed);
+  display.setCursor(25,45);
+  display.println("CLICK TO SAVE");
+  display.display();
+}
+
+double getLaserToWallDistance()
+{
+  // Set textSize only once
+  display.setTextSize(1);
+
+  // Initialize distance
+  double distance = 0;
+
+  while (true)
+  {
+    // Read joystick for speed
+    int yReading = analogRead(yPin);
+    double yOffCenter = calculateOffCenter(yReading);
+    double speed = offCenterToAngleChange(yOffCenter); 
+    distance += (speed * speedMultiplier * 0.2); // TODO: Change 0.2 with Y axis speed scale
+
+    // Fix distance
+    if (distance < 0)
+    {
+      distance = 0;
+    }
+
+    // Output Message
+    displayDistanceAndSpeedResult(distance, speed, String("Input wallDistance"));
+    
+    int buttonState = digitalRead(buttonPin);
+    if (!buttonState)
+    {
+      // Save Distance
+      return distance;
+    }
   }
 }
